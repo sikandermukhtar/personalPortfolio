421a8be58fb8374b33f82cc318c0b561e714f8af
Sikandar-chaudhry <sikandarmukhtar5@gmail.com>
2025-02-21 12:31:18 +0500

    Calling <Analytics /> component in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { Analytics } from "@vercel/analytics/react";
 
 const spaceGrotesk = Space_Grotesk({
   variable: "--font-space-grotesk",
@@ -25,6 +26,7 @@ export default function RootLayout({
         className={`${spaceGrotesk.variable} antialiased`}
       >
         <Navbar />
+        <Analytics/>
         {children}
       </body>
     </html>
